e22df48f9bcb9b8447c1c1aa47a873b5ee70c0eb
Taeho <65759092+bueeciel9@users.noreply.github.com>
2020-07-19 17:38:37 +0900

    Day_2

---
+++ weekly_study/Day2/day2.R
@@ -0,0 +1,13 @@
+ # 필요한 패캐지
+library(car)
+
+
+id = 1:5
+ age = c(29, 32, 47, 35, 23)
+ gender = c("f", "m", "m", "f", "f")
+ height = c(163, 177, 172, 157, 169)
+ DF1 = data.frame(id, age, gender, height)
+ DF2 = data.frame(id, age, gender, height, stringsAsFactors=FALSE)
+ DF1:3
+ 
+ 
